1df17361c51cd58ec6c5c1571d63a2286c94f7ec
Sion Berendsen <S.Berendsen9@student.han.nl>
2024-03-27 11:32:10 +0100

    Power timer als het goed is klaar. Als er geen bugs inzitten. Nog niet kunnen testen doordat de powers nog niet gespawned kunen worden

--- src/main/java/org/Game/PowerSpawner.java
+++ src/main/java/org/Game/PowerSpawner.java
@@ -1,6 +1,11 @@
 package org.Game;
 
+import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.entities.EntitySpawner;
+import org.Game.Entities.PipeObstacle;
+import org.Game.Entities.Powerups.PowerDoublePoints;
+import org.Game.Entities.Powerups.PowerTransparancy;
+import org.Game.Scenes.GameScene;
 
 public class PowerSpawner extends EntitySpawner {
 
@@ -10,14 +15,28 @@ public class PowerSpawner extends EntitySpawner {
 
     private int gameGap;
 
-    private int gameSpeed;
+    private double gameSpeed;
+
+    private GameScene gameScene;
+    private SkySourerGame skySourerGame;
 
     public PowerSpawner(double sceneHight, double sceneWidth, int interval, int gameSpeed, int gameGap) {
         super(interval);
+        this.sceneHight = sceneHight;
+        this.sceneWidth = sceneWidth;
+        this.gameSpeed = gameSpeed;
+        this.gameGap = gameGap;
     }
 
     @Override
     protected void spawnEntities() {
-
+        var powerDoublePoints = new PowerDoublePoints();
+        var powerTransparancy = new PowerTransparancy();
+        double randomDouble = Math.random() * 100;
+        if (randomDouble <= 7) {
+            spawn(powerDoublePoints);
+        } else if (randomDouble >= 93) {
+            spawn(powerTransparancy);
+        }
     }
 }
