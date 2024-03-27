2f9d61e02bd4ba294f00e7c2e95084334f7be291
murkimurk <mark04kamps@gmail.com>
2024-03-27 11:02:54 +0100

    grote van de sprite aangepast en beweging toegevoegd aan Power.java

--- src/main/java/org/Game/Entities/Power.java
+++ src/main/java/org/Game/Entities/Power.java
@@ -2,6 +2,7 @@ package org.Game.Entities;
 
 
 import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.entities.Collided;
 import com.github.hanyaeger.api.entities.SceneBorderCrossingWatcher;
 import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
@@ -16,6 +17,8 @@ public abstract class Power extends DynamicSpriteEntity implements Collided, Sce
     protected GameScene gameScene;
 
     public Power(Coordinate2D location, double gameSpeed, GameScene gameScene, String filename) {
-        super(filename, location);
+        super(filename, location, new Size(gameScene.getHeight() * 0.083));
+        this.gameScene = gameScene;
+        setMotion(gameSpeed, 270d);
     }
 }
